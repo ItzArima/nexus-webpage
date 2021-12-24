97082312a0832bb0cbf7eeca05b6aa08ffe240d2
ItzArima <skaypaprilia@gmail.com>
2021-12-24 17:51:36 +0100

    carousel and server section style animations added

--- resources/views/sub-extensions/carousel.blade.php
+++ resources/views/sub-extensions/carousel.blade.php
@@ -1,13 +1,18 @@
-<div class="gallery js-flickity"
-    data-flickity-options='{ "wrapAround": true }'>
-    @foreach (config('staff') as $staffer)
-        <div class="gallery-cell">
-            <img src="{{asset($staffer['avatar'])}}" width="30%" />
-            <div class="cell-text">
-                <h1>{{$staffer['nick']}}</h1>
-                <h3>{{$staffer['role']}}</h3>
+<div class="carousel-container">
+    <div class="desc-container">
+        <h1>I nostri <em>Staffer</em></h1>
+    </div>
+    <div class="gallery js-flickity"
+        data-flickity-options='{ "wrapAround": true }'>
+        @foreach (config('staff') as $staffer)
+            <div class="gallery-cell">
+                <img src="{{asset($staffer['avatar'])}}" width="30%" />
+                <div class="cell-text">
+                    <h1>{{$staffer['nick']}}</h1>
+                    <h3>{{$staffer['role']}}</h3>
+                </div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
+    </div>
 </div>
 
